c5b8ec626ad8166c4d69b66ee2c509d6707cabb8
zhuhongqiang <zhuhongqiang@soyoung.com>
2017-07-26 17:30:47 +0800

    测试111

---
+++ src/main/java/com/web/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.web;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.web.zhu.dao")
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
